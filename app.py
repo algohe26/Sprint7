0df3fed60ef6e87e86607a0daff06264b2b92e95
algohe26 <algohe39@gmail.com>
2026-01-12 16:20:46 -0600

    Agregada funcionalidad de Streamlit con histograma y scatter plot

--- app.py
+++ app.py
@@ -1,35 +1,21 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+car_data = pd.read_csv('vehicles_us.csv')  # porque app.py está en la raíz
 
-car_data = pd.read_csv(
-    'C:/Users/algoh/Repositorio GIT/Sprint7/vehicles_us.csv')  # leer los datos
+st.header('Datos de vehículos usados')
 
-# Encabezado de la aplicación
-st.header('Análisis Exploratorio de Datos de Venta de Coches')
-
-# --- SECCIÓN DE HISTOGRAMA ---
-st.write('### Histograma del Odómetro')
-# Opción con Botón
 hist_button = st.button('Construir histograma')
 
 if hist_button:
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    # Crear un histograma
-    fig_hist = px.histogram(car_data, x="odometer")
-    # Mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig_hist, use_container_width=True)
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
 
-# --- SECCIÓN DE GRÁFICO DE DISPERSIÓN ---
-st.write('### Gráfico de Dispersión: Precio vs. Año del Modelo')
-# Opción con Casilla de Verificación (Checkbox) - Desafío Extra
-build_scatter = st.checkbox('Construir gráfico de dispersión')
+scatter_button = st.button('Construir gráfico de dispersión')
 
-if build_scatter:
-    st.write('Visualizando la relación entre el precio y el año del modelo')
-    # Crear gráfico de dispersión
-    fig_scatter = px.scatter(car_data, x="model_year",
-                             y="price", title="Precio vs Año del Modelo")
-    # Mostrar gráfico
-    st.plotly_chart(fig_scatter, use_container_width=True)
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
